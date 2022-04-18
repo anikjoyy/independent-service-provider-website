f8052811cc126f3f35b08d94e44f9430d796a458
anikjoyy <anikjoy.cse@gmail.com>
2022-04-18 19:21:27 +0600

    added my services

--- src/Components/Home/Service/Service.js
+++ src/Components/Home/Service/Service.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
-const Service = () => {
-  return <div></div>;
+const Service = ({ service }) => {
+  const { id, name, price, img, description } = service;
+  return (
+    <div className='g-4 col-sm-12 col-md-6 col-lg-4'>
+      <div class='card h-100' style={{ width: '18rem' }}>
+        <img src={img} class='card-img-top' alt='...' />
+        <div class='card-body'>
+          <h5 class='card-title'>Name: {name}</h5>
+          <h6 className='card-title'>Price: {price}$</h6>
+          <p class='card-text'>Details: {description}</p>
+        </div>
+        <div class='card'>
+          <a href='#' className='btn btn-primary'>
+            Book Now
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Service;
--- src/Components/Home/Services/Services.js
+++ src/Components/Home/Services/Services.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Service from '../Service/Service';
 
 const Services = () => {
-  return <div></div>;
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    fetch('services.json')
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
+  return (
+    <div id='services' className='container mb-2'>
+      <h2 className='text-danger fw-bold text-center mt-5'> My Services</h2>
+      <div className='row'>
+        {services.map((service) => (
+          <Service key={service.id} service={service}></Service>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Services;

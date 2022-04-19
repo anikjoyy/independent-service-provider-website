a7effeb072bfb24609cba90f64c56025fedd903a
anikjoyy <anikjoy.cse@gmail.com>
2022-04-19 11:28:30 +0600

    created about me page

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
+import AboutImage from '../../Images/about/about.jpg';
+
 const About = () => {
-  return <div></div>;
+  return (
+    <div className='container mt-2 text-center bg-light'>
+      <img src={AboutImage} className='img-fluid w-25 rounded-circle' alt='' />
+      <h2 className='pt-2 text-dark fw-bold'>Anik Das</h2>
+      <p>
+        My goal is to become a full-stack web developer. To achieve my goal I
+        have already started working on it. Currently, I am doing a web
+        development course in programming hero. I am in a beginner-level
+        learning phase. I will work hard and practice more to achieve my goal.
+      </p>
+    </div>
+  );
 };
 
 export default About;

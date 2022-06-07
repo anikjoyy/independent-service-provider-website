cbe421816696ffc511f6c9584abfc5e680e23d01
anikjoyy <anikjoy.cse@gmail.com>
2022-06-07 15:03:48 +0600

    tried new things

--- src/Components/Home/ServiceDetails/ServiceDetails.js
+++ src/Components/Home/ServiceDetails/ServiceDetails.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Link, useParams } from 'react-router-dom';
+import useServiceDetails from '../../../hooks/useServiceDetails';
 
 const ServiceDetails = () => {
   const { serviceId } = useParams();
+  const [service] = useServiceDetails(serviceId);
+
   return (
     <div className='container mt-2 mb-5'>
       <h2 className='text-center text-dark'>Details Page</h2>
+      <h2 className='text-center text-dark'>{service.name}</h2>
+
       <h4 className='text-center text-danger'>
         Your package Id is: {serviceId}
       </h4>
